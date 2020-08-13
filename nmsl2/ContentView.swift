c7b45fd4b8725f13a71300fd3635e2b7f0c77d25
魏呈翰 <weichenghan@weichenghandeMacBook-Pro.local>
2020-08-13 16:40:36 +0800

    nm87

---
+++ nmsl2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nmsl2
+//
+//  Created by 魏呈翰 on 2020/8/13.
+//  Copyright © 2020 魏呈翰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
